a14908a3d490a98c9f0e01f7fbd704b63e05324a
lalugue <51893731+lalugue@users.noreply.github.com>
2021-04-24 15:52:14 +0800

    update movement, add getHasBall

--- Assets/Scripts/DogMovement.cs
+++ Assets/Scripts/DogMovement.cs
@@ -6,14 +6,18 @@ public class DogMovement : MonoBehaviour
 {
     public int velocity = 5;
     public float jumpForce = 20f;
-    bool canJump = true;
+    bool canJump = true; //for single jumping
+    int jumpCount = 0; //for double-jumping
+    int jumpLimit = 2;
+
+    bool hasBall = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow)){
             Vector3 position = this.transform.position;            
@@ -23,14 +27,26 @@ public class DogMovement : MonoBehaviour
             Vector3 position = this.transform.position;            
             this.transform.position += Vector3.right * velocity * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.UpArrow) && canJump){
-            canJump = false;
-            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        if (Input.GetKeyDown(KeyCode.UpArrow) && (jumpCount < jumpLimit)){
+            jumpCount++;
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
     }
 
-    void OnCollisionEnter2D(){
-        canJump = true;
+    void OnCollisionEnter2D(Collision2D col){
+        jumpCount = 0;
+        //canJump = true; //for single jumping
+
+        if(col.collider.gameObject.name == "Ball"){
+            hasBall = true;
+            GameObject.Destroy(col.collider.gameObject);
+        }
     }
+
+    public bool getHasBall(){
+        return hasBall;
+    }
+
+
 
 }
