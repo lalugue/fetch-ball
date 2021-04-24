c4d36b576df22a436b779c58b564c24bbfd5d4d3
lalugue <51893731+lalugue@users.noreply.github.com>
2021-04-24 15:51:40 +0800

    detect dog with ball

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    public GameObject dog;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){            
+        if(col.gameObject.name == "Dog"){            
+            if(dog.GetComponent<DogMovement>().getHasBall())
+            {
+                Debug.Log("Good boy!");
+            }
+        }
+    }
+}
